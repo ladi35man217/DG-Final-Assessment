471c617a8a8b566bf3696b6b70793ccf0301f9e1
Daniel <danielgiemz@outlook.com>
2024-10-15 16:01:04 +1100

    Created data classes to match JSON file

--- Androidbasicapp-Android-tests/app/src/main/java/com/vu/androidbasicapp/home/data/ApiResponse.kt
+++ Androidbasicapp-Android-tests/app/src/main/java/com/vu/androidbasicapp/home/data/ApiResponse.kt
@@ -1,6 +1,19 @@
 package com.vu.androidbasicapp.home.data
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class ApiResponse(
     val entities: List<Entity>,
     val entityTotal: Int
+)
+
+@JsonClass(generateAdapter = true)
+data class Entity(
+    @Json(name = "description") val description: String,
+    @Json(name = "dishName") val dishName: String,
+    @Json(name = "mainIngredient") val mainIngredient: String,
+    @Json(name = "mealType") val mealType: String,
+    @Json(name = "origin") val origin: String
 )
--- Androidbasicapp-Android-tests/app/src/main/java/com/vu/androidbasicapp/home/data/Entity.kt
+++
@@ -1,9 +0,0 @@
-package com.vu.androidbasicapp.home.data
-
-data class Entity(
-    val description: String,
-    val dishName: String,
-    val mainIngredient: String,
-    val mealType: String,
-    val origin: String
-)
