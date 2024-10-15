7af6bbec494062e1a3029f946cc5eb310bb0c9d0
Daniel <danielgiemz@outlook.com>
2024-10-15 15:50:11 +1100

    API Request Import from JSON

---
+++ Androidbasicapp-Android-tests/app/src/main/java/com/vu/androidbasicapp/home/data/ApiResponse.kt
@@ -0,0 +1,6 @@
+package com.vu.androidbasicapp.home.data
+
+data class ApiResponse(
+    val entities: List<Entity>,
+    val entityTotal: Int
+)
---
+++ Androidbasicapp-Android-tests/app/src/main/java/com/vu/androidbasicapp/home/data/Entity.kt
@@ -0,0 +1,9 @@
+package com.vu.androidbasicapp.home.data
+
+data class Entity(
+    val description: String,
+    val dishName: String,
+    val mainIngredient: String,
+    val mealType: String,
+    val origin: String
+)
